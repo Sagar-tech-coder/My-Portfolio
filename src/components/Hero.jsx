145d27def6b69849aa6b6d44da0c67f40b852f7a
Sagar <sagarpatra641@gmail.com>
2024-08-10 13:57:21 +0530

    sillks added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,19 @@
 import "./Hero.css";
 import Me from "../assets/Me.png";
+import java from "../assets/java.svg";
+import html from "../assets/html.svg";
+import css from "../assets/css.svg";
+import sass from "../assets/sass.svg";
+import js from "../assets/js.svg";
+import tailwind from "../assets/tailwindcss.svg";
+import react from "../assets/react.svg";
+import mysql from "../assets/mysql.svg";
+import git from "../assets/git.svg";
+import github from "../assets/github.svg";
+// import allskills from "../assets/allskills.png";
+// import TicTac from "../assets/TicTac.png";
+// import EcoLabs from "../assets/EcoLabs.png";
+// import cal from "../assets/cal.png";
 // import Arrow from "../assets/Arrow.png";
 // import { Cursor, Typewriter } from "react-simple-typewriter";
 
@@ -37,7 +51,7 @@ const Hero = () => {
         {/* about me */}
         <div className="about-main">
           <div className="about-head">A Bit About Me 🚀</div>
-          <div className="about-desc">
+          <p className="about-desc">
             I'm a final year student of Bachelor's in Computer Applicaton
             (B.C.A) from Techno India Institute of Technology (TiiT) Kolkata,
             West Bengal, India. And also a self-taught Front-end Developer with
@@ -48,11 +62,52 @@ const Hero = () => {
             good design, technology, and innovation in all my projects, which I
             like to accompany from the first idea to release. Currently, I'm
             focused on the backend development.
-          </div>
+          </p>
         </div>
         {/* Tools and Technology */}
         <div className="tools-main">
           <div className="tools-head">Tools and Technology 🚀</div>
+          <p className="tools-desc">
+            Using a combination of cutting-edge technologies and reliable
+            open-source software I build user-focused, performant websites for
+            smartphones, tablets, and desktops.
+          </p>
+          {/* tools icons */}
+          <div class="tools-grid">
+            <div class="box" id="box1">
+              <img src={java} alt="" />
+              <span>JAVA</span>
+            </div>
+            <div class="box" id="box2">
+              <img src={html} alt="" /> <span>HTML</span>
+            </div>
+            <div class="box" id="box3">
+              <img src={css} alt="" /> <span>CSS</span>
+            </div>
+            <div class="box" id="box4">
+              <img src={sass} alt="" /> <span>Sass</span>
+            </div>
+            <div class="box" id="box5">
+              <img src={js} alt="" /> <span>JavaScript</span>
+            </div>
+            <div class="box" id="box6">
+              <img src={tailwind} alt="" />
+              <span>Tailwind CSS</span>
+            </div>
+            <div class="box" id="box7">
+              <img src={react} alt="" />
+              <span>React.js</span>
+            </div>
+            <div class="box" id="box8">
+              <img src={mysql} alt="" /> <span>MySQL</span>
+            </div>
+            <div class="box" id="box9">
+              <img src={git} alt="" /> <span>Git</span>
+            </div>
+            <div class="box" id="box10">
+              <img src={github} alt="" /> <span>GitHub</span>
+            </div>
+          </div>
         </div>
       </section>
     </>
